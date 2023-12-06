f1a328af693084d9bcdcbf422afec270cd539d4b
Slackz0r <gustav.aldenberg@gmail.com>
2023-12-06 11:09:05 +0100

    questions object

---
+++ index.js
@@ -0,0 +1,47 @@
+let questions = [
+  {
+    question: "I frankrike är det olagligt att döpa en gris till 'Napoleon'",
+    answer: true,
+  },
+  {
+    question: "Vilken av följande är INTE en karaktär i 'Sagan om Ringen'",
+    answer: "Beowulf",
+  },
+  {
+    question: "Vad heter Gustav i mellannamn?",
+    answer: "Lars Gunnar",
+  },
+  {
+    question: "Vilka av följande ingredienser innehåller en 'Pasta Carbonara'?",
+    answer: "Bacon",
+    answer2: "Äggula",
+  },
+  {
+    question: "Banan är en baljväxt",
+    answer: false,
+  },
+  {
+    question: "Vilka av följande är INTE svenska kungar?",
+    answer: "Kung Hans",
+    answer2: "Kung Erland",
+  },
+  {
+    question:
+      "Vad heter den mest grundläggande vågformen, som alla andra är uppbyggda utav?",
+    answer: "Sinus",
+  },
+  {
+    question: "Vilket djur bet Ozzy Osbourne av huvudet på under en konsert?",
+    answer: "Fladdermus",
+  },
+  {
+    question: "Vem är 'E-type'?",
+    answer: "Långhårig eurodisco legend",
+  },
+  {
+    question: "Vad är 'E-type' väldigt intresserad av?",
+    answer: "Vikingar",
+  },
+];
+
+let score = 0;
